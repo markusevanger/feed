730eda4e660315bc86af2420a26e317c1b121442
Markus <markusevanger@gmail.com>
2025-03-08 01:00:38 +0100

    updt postcss config

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -2,19 +2,7 @@
 
 /** @type {import('tailwindcss').Config} */
 const config = {
-
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-    './app/**/*.{js,ts,jsx,tsx}',
-  ],
-
-
   plugins: ["@tailwindcss/postcss"],
-
-
 };
-
-
 
 export default config;
