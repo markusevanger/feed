7383861f23b8a42f22964843308503f5e6095256
markus <markus@kult.design>
2026-01-18 22:16:40 +0100

    upload to media server

--- apps/web/next.config.ts
+++ apps/web/next.config.ts
@@ -3,6 +3,10 @@ import type { NextConfig } from "next";
 const isDev = process.env.NODE_ENV === 'development';
 
 const nextConfig: NextConfig = {
+  env: {
+    SANITY_STUDIO_MEDIA_SERVER_URL: process.env.SANITY_STUDIO_MEDIA_SERVER_URL,
+    SANITY_STUDIO_MEDIA_API_KEY: process.env.SANITY_STUDIO_MEDIA_API_KEY,
+  },
   images: {
     dangerouslyAllowLocalIP: isDev,
     remotePatterns: [
